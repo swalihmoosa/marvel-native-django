1c82e2b2186a9956dfcb2727a7461e425ec27945
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-21 15:59:33 +0530

    serializer | ongoing

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from api.views import user_list
+
 app_name = "api"
 
 
 urlpatterns = [
-
+    path('users', user_list, name="user_list")
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from api.serializers import UserSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+
+
+    
